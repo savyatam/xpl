e54afc27c71da621dbcf481ef12321e1b9b81ca2
savyatam <ysav1999@rediffmail.com>
2020-01-08 22:40:12 +0530

    frontendandbackend

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const app=express();
+var http = require('http').Server(app);
+var port = process.env.PORT || 3000;
+/*app.use(express.static('public'));
+app.get('/',function(req,res) {
+  res.sendFile(__dirname+'/public/index.html');
+
+});*/
+app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
