7b43c621d25991d068c00ec78ac4bc133194f5f2
savyatam <ysav1999@rediffmail.com>
2020-01-08 23:49:11 +0530

    try

--- index.js
+++ index.js
@@ -8,15 +8,11 @@ app.get('/',function(req,res) {
   res.sendFile(__dirname+'/public/index.html');
 
 });*/
-app.use(express.static('frontend/build'));
+app.use(express.static('client/build'));
 
-  /*app.get('/yeah', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });*/
-  app.get('/yeah',function(req,res) {
-    res.send('erhd');
-
-  });
+  app.get('/yeah', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
