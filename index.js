6337c816d013a9f1a26de70002d64f710040c853
savyatam <ysav1999@rediffmail.com>
2020-01-08 22:51:22 +0530

    add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const app=express();
+const path = require('path');
 var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
 /*app.use(express.static('public'));
@@ -9,7 +10,7 @@ app.get('/',function(req,res) {
 });*/
 app.use(express.static('frontend/build'));
 
-  app.get('*', (req, res) => {
+  app.get('/yeah', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 http.listen(port, function(){
